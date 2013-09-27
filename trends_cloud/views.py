32f1409d9ba6eee91872d9a9f498e42eabd2e25c
Tina Huang <tina@twitter.com>
2013-09-27 10:58:13 -0700

    remove some unused code

--- trends_cloud/views.py
+++ trends_cloud/views.py
@@ -3,7 +3,5 @@ from django.http import HttpResponse
 import datetime
 
 def home(request):
-    now = datetime.datetime.now()
     context = {}
     return render(request, 'trends_cloud/main.html', context)
-
